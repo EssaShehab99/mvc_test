ea0de4894bd43052f8e79eb854f5b85c827222ed
EssaShehab <essa323371@gmail.com>
2022-01-05 01:25:55 +0300

    initi

---
+++ Models/employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CityNo { get; set; }
+        public int DeptNo { get; set; }
+        public int HireDate { get; set; }
+        public int JobId { get; set; }
+        public int Salary { get; set; }
+        public int IsActive { get; set; }
+    }
+}
