d1d30434a2299022fb565280cc34b87a30d8ded9
EssaShehab <essa323371@gmail.com>
2022-01-30 10:54:40 +0300

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace WebApplication3.Controllers
         {
             return View();
         }
+        public IActionResult Inner()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/city.cs
+++ Models/city.cs
@@ -9,6 +9,5 @@ namespace WebApplication3.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
-
     }
 }
